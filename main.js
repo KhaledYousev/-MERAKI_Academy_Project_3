8d28c06ad7a0dc55c83a41923712721e83b7db68
KhaledYousev <mr.khaledyusuf@gmail.com>
2021-05-16 18:41:29 +0300

    1. getAllArticles

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
+app.use(express.json());
 
+const articles = [
+    {
+    id: 1,
+    title: 'How I learn coding?',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+    {
+    id: 2,
+    title: 'Coding Best Practices',
+    description:
+    'Lorem, ipsum dolor sit, Quam, mollitia.',
+    author: 'Besslan',
+    },
+    {
+    id: 3,
+    title: 'Debugging',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+    ];
 
+    //ticket:1//
+    app.get("/articles", (req, res) => {
+        res.status(200);
+        res.json(articles);
+      });
 
 
 
